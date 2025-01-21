167f1a9fa81d3f2d9a291b4b45dbb95cfe9a4848
Ayush Kumar <ayushsuperstar48@gmail.com>
2025-01-21 15:08:14 +0530

    .

--- src/routes/[...slug]/+server.js
+++ src/routes/[...slug]/+server.js
@@ -3,7 +3,8 @@ import { TMDB_BASE_URL, TMDB_READ_ACCESS_KEY } from "$env/static/private";
 export async function GET({ url }) {
   const pathname = url.pathname;
 
-  const apiUrl = `${TMDB_BASE_URL}${pathname}`;
+  const apiUrl = TMDB_BASE_URL + pathname;
+  console.log(apiUrl);
   const response = await fetch(apiUrl, {
     headers: {
       Authorization: `Bearer ${TMDB_READ_ACCESS_KEY}`,
@@ -11,6 +12,7 @@ export async function GET({ url }) {
   });
 
   if (!response.ok) {
+    console.log(response);
     return new Response(JSON.stringify({ error: "Failed to fetch data" }), {
       status: response.status,
       headers: {
